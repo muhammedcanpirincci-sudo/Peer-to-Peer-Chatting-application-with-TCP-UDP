363899e1cc07f913720267bf475d6ae27b91c8ca
basilelkhalifa <basilosama.2000@gmail.com>
2022-01-10 01:49:35 +0300

    added a test client

---
+++ testclient.py
@@ -0,0 +1,27 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISC"
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+
+while True:
+    msg = input("send message: ")
+    send(msg)
+    if msg == DISCONNECT_MESSAGE:
+        break
