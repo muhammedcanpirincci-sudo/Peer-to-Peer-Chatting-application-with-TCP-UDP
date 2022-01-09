b2689a600bf9185e251c21faacccc1c44302ea66
basilelkhalifa <basilosama.2000@gmail.com>
2022-01-10 01:50:05 +0300

    moved files

--- Server.py
+++
@@ -1,7 +0,0 @@
-from threading import Thread
-import servers.TCPserver as tcp_server
-import servers.UDPServer as udp_server
-
-if __name__ == '__main__':
-    Thread(target = tcp_server.receive).start()
-    Thread(target = udp_server.listen_clients).start()
